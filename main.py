1588cc2e1fc9ca51e15d3a7486fa1c8975d929c0
Carl Harris <elgoogemail2007@gmail.com>
2016-11-21 04:31:11 -0500

    added camera, on-screen debugging, and art

--- main.py
+++ main.py
@@ -6,13 +6,169 @@ pygame.init()
 SCREEN_WIDTH=800
 SCREEN_HEIGHT=600
 FRICTION = .02
+
+LAYER_OVERLAY = 1
+LAYER_CAMERA = 2
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-background = pygame.image.load("img/background.bmp").convert()
+def Font(size=32):
+    return pygame.font.Font(None, size)
 
-class Player(object):
+
+class World(object):
+
+    def __init__(self, camera=None, *sprites):
+        if camera is None:
+            camera = Camera()
+        self.camera = camera
+        self.sprites = []
+        self.add(*sprites)
+        self.debug = []
+
+    def add(self, *sprites):
+        for sprite in sprites:
+            self.sprites.append(sprite)
+
+    def ordered_sprites(self):
+        def layer(sprite):
+            if hasattr(sprite, 'layer'):
+                return sprite.layer
+            return 0
+
+        return sorted(self.sprites, key=layer)
+
+    def draw(self, surface):
+        offset = -self.camera.rect.x, -self.camera.rect.y
+
+        messages = []
+        messages.append('offset %s' % (offset, ))
+
+        screen_rect = surface.get_rect()
+        if self.debug:
+            debugfont = Font(24)
+
+        for sprite in self.ordered_sprites():
+            if hasattr(sprite, 'image') and hasattr(sprite, 'rect'):
+                if hasattr(sprite, 'overlay') and sprite.overlay:
+                    rect = sprite.rect
+                else:
+                    rect = sprite.rect.move(offset)
+
+                surface.blit(sprite.image, rect)
+
+                if sprite in self.debug:
+                    s = 'world: %s, screen: %s' % (sprite.rect.topleft, sprite.rect.move(offset).topleft)
+                    image = debugfont.render(s, True, (255, 255, 255))
+                    rect.size = image.get_rect().size
+                    surface.blit(image, rect.clamp(screen_rect))
+
+        for obj in self.debug:
+            if isinstance(obj, pygame.Rect):
+                pygame.draw.rect(surface, (255, 255, 255), obj.move(offset), 1)
+
+        if self.camera in self.debug:
+            messages.append('camera: ' + str(self.camera.rect))
+
+        for obj in self.debug:
+            if callable(obj):
+                messages.append(obj())
+
+        y = 0
+        for msg in messages:
+            image = debugfont.render(msg, True, (255, 100, 100))
+            rect = image.get_rect()
+            rect.right = screen_rect.right - 10
+            rect.y = y
+
+            surface.blit(image, rect)
+            y += rect.height
+
+    def update(self, *args):
+        for sprite in self.sprites:
+            sprite.update(*args)
+        self.camera.update(*args)
+
+
+class FramerateSprite(pygame.sprite.Sprite):
+
+    def __init__(self, clock, *groups):
+        super(FramerateSprite, self).__init__(*groups)
+        self.rect = pygame.Rect(0, 0, 0, 0)
+        self._images = {}
+        self._font = Font()
+        self.clock = clock
+        self.layer = 99
+        self.overlay = True
+
+    @property
+    def image(self):
+        framerate = self.clock.get_fps()
+        if not framerate in self._images:
+            self._images[framerate] = self._font.render(str(framerate), True, (255,255,255))
+        return self._images[framerate]
+
+
+class TextSprite(pygame.sprite.Sprite):
+
+    def __init__(self, text):
+        self.text = text
+        self.position = (0, 0)
+
+    def update(self, dt):
+        self.image = Font().render(text, True, (255, 255, 255))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.position
+
+
+class Camera(pygame.sprite.Sprite):
+
+    def __init__(self, limit=None):
+        super(Camera, self).__init__()
+        self.rect = pygame.Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.lock = None
+
+        self.focus = pygame.Rect(0, 0, 300, 100)
+        self.focus.center = self.rect.center
+
+        self.limit = limit
+
+    def update(self, dt):
+        if self.lock is None:
+            return
+
+        if self.focus.contains(self.lock.rect):
+            return
+
+        if self.focus.left > self.lock.rect.left:
+            self.focus.left = self.lock.rect.left
+
+        elif self.focus.right < self.lock.rect.right:
+            self.focus.right = self.lock.rect.right
+
+        if self.focus.top > self.lock.rect.top:
+            self.focus.top = self.lock.rect.top
+
+        elif self.focus.bottom < self.lock.rect.bottom:
+            self.focus.bottom = self.lock.rect.bottom
+
+        self.rect.center = self.focus.center
+
+        if self.limit is not None:
+            self.rect.clamp_ip(self.limit)
+
+
+class Field(pygame.sprite.Sprite):
 
     def __init__(self):
+        self.image = pygame.image.load("img/field.png").convert()
+        self.rect = self.image.get_rect()
+
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super(Player, self).__init__()
         self.px = 400
         self.py = 300
         self.vx = 0
@@ -20,7 +176,12 @@ class Player(object):
         self.ax = 0
         self.ay = 0
 
-        self.image = pygame.image.load("img/player.bmp").convert()
+        self.image = pygame.image.load("img/player.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        
+        self.layer = 1
+
+        self.limit = None
 
     def get_input(self, dt):
         keystate = pygame.key.get_pressed()
@@ -70,11 +231,47 @@ class Player(object):
         self.px += self.vx * dt
         self.py += self.vy * dt
 
+        self.rect.topleft = (self.px, self.py)
+
+        if self.limit:
+            if not self.limit.contains(self.rect):
+                if self.rect.left < self.limit.left or self.rect.right > self.limit.right:
+                    self.vx = 0
+                if self.rect.top < self.limit.top or self.rect.bottom > self.limit.bottom:
+                    self.vy = 0
+                self.rect.clamp_ip(self.limit)
+                self.px = self.rect.x
+                self.py = self.rect.y
+
     def render(self, screen):
         screen.blit(self.image, (self.px, self.py))
 
+
+world = World()
+
+#background = pygame.image.load("img/field.png").convert()
+field = Field()
+
 player = Player()
+player.limit = pygame.Rect(24, 0, 644 - 24, 2440)
+
+world.camera.lock = player
+world.camera.limit = field.rect.inflate(250, 250)
+
 clock = pygame.time.Clock()
+
+framerate = FramerateSprite(clock)
+
+world.add(player, framerate, field)
+
+world.debug.append(player)
+world.debug.append(world.camera)
+world.debug.append(world.camera.focus)
+world.debug.append(player.rect)
+
+world.debug.append(lambda: 'player.vx: {:.2f}'.format(player.vx, ))
+world.debug.append(lambda: 'player.vy: {:.2f}'.format(player.vy, ))
+world.debug.append(lambda: 'player.limit: %s' % (player.limit, ))
 
 done = False
 while not done:
@@ -118,12 +315,16 @@ while not done:
             #    player.ax = 0
     
     
-    #render phase
-    screen.blit(background, (0,0))
-    player.render(screen)
-    
     #logic phase
-    player.update(dt)
+    #world.clear(screen, background)
+    world.update(dt)
+    #player.update(dt)
+    #camera.update(dt)
+    
+    #render phase
+    screen.fill((0,0,0))
+    world.draw(screen)
+    #player.render(screen)
     
     pygame.display.flip()
 
