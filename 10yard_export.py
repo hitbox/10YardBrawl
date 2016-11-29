84ab3eabe2d761d3847d1013a827920b213c5d58
Carl Harris <elgoogemail2007@gmail.com>
2016-11-29 03:32:45 -0500

    updated gimp plugin to export inside the project dir, added the gimp sprite sheet

--- 10yard_export.py
+++ 10yard_export.py
@@ -9,78 +9,99 @@ import tempfile
 
 from gimpfu import *
 
-# I don't care much for gimp thinking they clash names with the built-in debugger.
+# I don't care much for gimp thinking they can clash names with the built-in debugger.
 gpdb = pdb
 del pdb
 
-#TODO: ensure dir exists
-tempdir = os.path.join(tempfile.gettempdir(), '10 Yard Brawl')
+NAME = 'export'
+LOGGING_LEVEL = logging.INFO
 
-class Window(gtk.Window):
-    """
-    Convenience window class.
-    """
+HELP = 'Exports the layers in a group layer named "sprites".'
 
-    def __init__(self):
-        super(Window, self).__init__(gtk.WINDOW_TOPLEVEL)
+class DirChooserButton(gtk.FileChooserButton):
+
+    def __init__(self, title, backend=None):
+        super(DirChooserButton, self).__init__(title, backend)
+        self.set_action(gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER)
+
+
+class ExportWindow(gtk.Window):
+
+    def __init__(self, image, dest):
+        super(ExportWindow, self).__init__(gtk.WINDOW_TOPLEVEL)
+
+        self.set_title('Export Sprites')
+
+        self.image = image
+        self.dest = dest
+        self.log = logging.getLogger(NAME)
+        self.log.setLevel(LOGGING_LEVEL)
+        self.log.info('__init__')
 
         self.connect('delete_event', self.delete_event)
         self.connect('destroy', self.destroy)
 
+        vbox = gtk.VBox()
+
+        vbox.pack_start(gtk.Label(HELP))
+
+        self.label = gtk.Label(self.dest)
+        vbox.pack_start(self.label)
+
+        button = gtk.Button('Destination Directory')
+        button.connect('clicked', self.choose_dest_dir)
+        vbox.pack_start(button)
+
+        confirm = gtk.Button('OK')
+        confirm.connect('clicked', self.confirm_clicked)
+        vbox.pack_start(confirm)
+
+        self.add(vbox)
         self.show_all()
+
+    def choose_dest_dir(self, widget):
+        chooser = gtk.FileChooserDialog(
+                'Choose Destination Directory',
+                self,
+                gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER,
+                (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+        chooser.set_current_folder(self.dest)
+
+        response = chooser.run()
+        if response == gtk.RESPONSE_OK:
+            self.dest = chooser.get_current_folder()
+        self.label.set_text(self.dest)
+        chooser.destroy()
 
     def delete_event(self, widget, event, data=None):
         return False
 
     def destroy(self, widget, data=None):
-        gtk.main_quit()
+        self.quit()
 
-    def main(self):
-        gtk.main()
+    def confirm_clicked(self, widget):
+        self.export_sprites()
+        self.quit()
 
+    def error(self, text):
+        message = gtk.MessageDialog(parent=self, 
+                            flags=0, 
+                            type=gtk.MESSAGE_INFO, 
+                            buttons=gtk.BUTTONS_NONE, 
+                            message_format=None)
+        message.set_markup(text)
+        self.log.info(text)
+        message.run()
 
-def is_int(s):
-    try:
-        int(s)
-    except ValueError:
-        return False
-    else:
-        return True
-
-class ExportWindow(Window):
-
-    def __init__(self, image):
-        super(ExportWindow, self).__init__()
-        self.image = image
-        self.log = logging.getLogger('10yardbrawl.ExportWindow')
-        self.log.setLevel(logging.INFO)
-
-        self.button = gtk.Button('Debug')
-        self.button.connect('clicked', self.debug, None)
-        self.add(self.button)
-
-        self.show_all()
-
-    def debug(self, widget, data=None):
-        try:
-            return self._debug()
-        except:
-            self.log.exception('Exception')
-
-    def _debug(self):
+    def export_sprites(self):
+        self.log.info('export_sprites')
         tempimage = self.image.duplicate()
 
         sprites_group = find_sprites_group(tempimage)
 
         if not sprites_group:
-            self.log.info('"sprites" LayerGroup not found.')
+            self.error('"sprites" LayerGroup not found.')
             return
-
-        for dirpath, dirnames, filenames in os.walk(tempdir):
-            for filename in filenames:
-                if filename.endswith('.png'):
-                    os.remove(os.path.join(dirpath, filename))
-                    self.log.info('removed: %s', os.path.join(dirpath, filename))
 
         for layer in walklayers(sprites_group):
             if isinstance(layer, gimp.GroupLayer) or ' ' in layer.name:
@@ -88,18 +109,12 @@ class ExportWindow(Window):
 
             tokens = layer.name.split('-')
 
-            if is_int(tokens[-1]):
-                # XXX: Not dealing with layers that need to be combined and aligned yet.
-                continue
-
             # copy and paste layer to new image
             gpdb.gimp_edit_copy(layer)
             export_image = gpdb.gimp_edit_paste_as_new()
 
             filename = '-'.join(tokens) + '.png'
-            fullpath = os.path.join(tempdir, filename)
-
-            #gpdb.plug_in_zealouscrop(export_image, export_image.layers[0])
+            fullpath = os.path.join(self.dest, filename)
 
             INTERLACE = False
             COMPRESSION = 9
@@ -113,6 +128,26 @@ class ExportWindow(Window):
                                SAVE_BACKGROUND_COLOR, SAVE_GAMMA,
                                SAVE_LAYER_OFFSET, SAVE_CREATION_TIME,
                                SAVE_COLOR_TRANSPARENT)
+
+            self.log.info('%s exported to %s', layer, fullpath)
+
+    def main(self):
+        gtk.main()
+
+    def quit(self):
+        gtk.main_quit()
+
+
+def default_directory():
+    return os.path.join(tempfile.gettempdir(), '10 Yard Brawl')
+
+def is_int(s):
+    try:
+        int(s)
+    except ValueError:
+        return False
+    else:
+        return True
 
 
 def find_sprites_group(image_or_layers):
@@ -135,43 +170,45 @@ def walklayers(arg):
                 yield layer
 
 def export(image, drawable):
-    logging.basicConfig(level=logging.INFO,
-                        filename=os.path.join(tempdir, '10yardbrawl.txt'))
-    logging.getLogger('10yardbrawl').setLevel(logging.INFO)
-    log = logging.getLogger('10yardbrawl.export')
 
-    log.info('started')
+    if image.filename:
+        dest = os.path.join(os.path.dirname(os.path.dirname(image.filename)), 'img')
+    else:
+        dest = default_directory()
+
+    logging.basicConfig(level=logging.INFO,
+                        filename=os.path.join(dest, NAME + '.log'))
+    logging.getLogger('').setLevel(LOGGING_LEVEL)
+    log = logging.getLogger(NAME)
 
     try:
-        window = ExportWindow(image)
-        window.main()
+
+        log.info('started')
+        log.info('image: %s', image)
+        log.info('drawable: %s', drawable)
+
+        exportwindow = ExportWindow(image, dest)
+        exportwindow.main()
+
     except:
-        log.exception('Exception')
+        log.exception('An exception occured.')
+    else:
+        log.info('done')
 
-    log.info('done')
+# NOTES: Gimp fails if a GroupLayer object is the drawable passed to `export`
 
-# register(proc_name, blurb, help, author, copyright, date, label, imagetypes,
-#          params, results, function, menu=None, domain=None, on_query=None,
-#          on_run=None)
-
-# NOTES: 1. image and drawable seem to be automatically passed
-#        2.
-#
-# TODO: 1. Selectable layer, automatically picking one named "sprites".
-#       2. Get rid of custom GTK Window, this should be possible with the register interface.
-#       3. Handle irregular layers.
-
-register("export", # proc_name
-         "ExportWindow 10 Yard Brawl", # blurb
-         "ExportWindow 10 Yard Brawl sprites.", # help
-         "CincyPy", # author
-         "copyright", # copyright
-         "year", # date
-         "<Image>/Sprites/Export...", # label
-         "*", # imagetypes
-         [], # params
-         [], # results
-         export, # function
-         )
+register('export', # proc_name
+         HELP, # blurb
+         HELP, # help
+         'CincyPy', # author
+         'copyright', # copyright
+         'year', # date
+         '<Image>/Export/Sprites...', # label
+         '*', # imagetypes
+         # params
+         [],
+         # results
+         [],
+         export)
 
 main()
